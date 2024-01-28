6a1c5823b51ecaf0e2e136a9c2501d85b5c2fc15
Amer M <67227693+Amer328@users.noreply.github.com>
2024-01-28 14:08:38 +0000

    Update qa.py

--- qa.py
+++ qa.py
@@ -10,21 +10,19 @@ api_key = st.secrets["OPENAI_API_KEY"]
 # Set the OpenAI key
 openai.api_key = api_key
 
-
 def create_prompt(context,query):
     header = "Answer the question with as much detail as possible using the provided context and support your answer with paragraphs and bullet points. If no answer is generated, print 'Sorry insufficient data to answer query' \n"
     return header + context + "\n\n" + query + "\n"
-
 
 def generate_answer(prompt):
     client = OpenAI()
 
     response = client.chat.completions.create(
-    # model="gpt-3.5-turbo-16k",
-    model="gpt-4-turbo-preview",
+    model="gpt-3.5-turbo-16k",
+    # model="gpt-4-turbo-preview",
     messages=[{"role": "user", "content": prompt}],
     temperature=0.1,
-    # max_tokens=12000,
+    max_tokens=12000,
     top_p=1,
     frequency_penalty=0,
     presence_penalty=0
