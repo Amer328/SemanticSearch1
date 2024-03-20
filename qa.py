904527c0a66753b0b9d48e492b4f1b3b9e95d316
Amer M <67227693+Amer328@users.noreply.github.com>
2024-03-20 21:44:59 +0000

    Update qa.py

--- qa.py
+++ qa.py
@@ -23,7 +23,7 @@ def generate_answer(prompt):
     # model="gpt-4-turbo-preview",
     messages=[{"role": "user", "content": prompt}],
     temperature=0.1,
-    max_tokens=2000,
+    max_tokens=12000,
     top_p=1,
     frequency_penalty=0,
     presence_penalty=0
