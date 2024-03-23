b6fca95773b18eefdad7477a821c8459ed302de1
Amer M <67227693+Amer328@users.noreply.github.com>
2024-03-23 15:12:17 +0000

    Update qa.py

--- qa.py
+++ qa.py
@@ -19,7 +19,7 @@ def generate_answer(prompt):
     client = OpenAI()
 
     response = client.chat.completions.create(
-    model="gpt-3.5-turbo-1106" #"gpt-3.5-turbo-16k",
+    model="gpt-3.5-turbo-1106", #"gpt-3.5-turbo-16k",
     # model="gpt-4-turbo-preview",
     messages=[{"role": "user", "content": prompt}],
     temperature=0.1,
